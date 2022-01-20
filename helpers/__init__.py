36760c887fc3e6d872b7962a9ceaaebed705db84
Cubic-Knight <97886501+Cubic-Knight@users.noreply.github.com>
2022-01-21 00:34:33 +0100

    Update __init__.py

--- helpers/__init__.py
+++ helpers/__init__.py
@@ -1,8 +1,11 @@
 from .paths import osu_fp, complete_path
-from .vector import Vector
+from .parsing import split_get
+from .plane_classes import Vector, CartesianLine
+from .plane_functions import segment_fraction, bezier
 
 __all__ = [
-    'complete_path',
-    'Vector',
+    'complete_path', 'split_get',
+    'Vector', 'CartesianLine',
+    'segment_fraction', 'bezier',
     'osu_fp'
 ]
