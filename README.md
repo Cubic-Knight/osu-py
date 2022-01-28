735ac6fc1f801daff196bb0e36b7911965f52353
Cubic-Knight <root.cubic.knight@gmail.com>
2022-01-29 00:51:48 +0100

    Revert accidents

--- README.md
+++ README.md
@@ -1,10 +1,3 @@
 # osu-py
 
-TODO:
-- Implement helpers to get rid of my_tools dependency:
-    - segment_fraction
-    - split_get
-    - and others in tools/slider_analyser.py
-- Make better looking code in some files:
-    - ...
-- ... 
+Currently am trying to make my code look better
--- __init__.py
+++ __init__.py
@@ -1,7 +1,7 @@
-from .beatmap_classes import *
-from .storyboard_classes import *
-from .beatmap_compressor import compress_beatmap
-from .beatmap_decompressor import decompress_beatmap
+from .beatmap import *
+from .helpers import osu_fp
+from .replay import *
+from .tools import *
 
 __all__ = [
     'Settings', 'GeneralSettings', 'EditorSettings', 'MetadataSettings', 'DifficultySettings', 'ColorSettings',
@@ -10,8 +10,20 @@ __all__ = [
     'SpriteCommand', 'Fade', 'Move', 'MoveX', 'MoveY', 'Scale', 'VectorScale', 'Rotate', 'Color', 'Parameter',
     'HitSample', 'TimingPoint',
     'HitObject', 'Circle', 'Slider', 'SliderAdditionalData', 'Spinner', 'Hold',
-    'Beatmap', 'BeatmapError',
+    'Beatmap', 'Replay', 'ReplayFrame',
+    'BeatmapError',
 
     'get_hit_object_class', 'get_event_class',
+    'decompress_replay', 'compress_replay',
     'decompress_beatmap', 'compress_beatmap',
+    'analyse_slider', 'analyse_beatmap',
+
+    'ar_to_ms', 'ms_to_ar',
+    'od_to_win300', 'od_to_win100', 'od_to_win50', 'win300_to_od', 'win100_to_od', 'win50_to_od',
+    'cs_to_radius', 'radius_to_cs',
+
+    'M1', 'M2', 'K1', 'K2', 'SMOKE',
+    'PLAYFIELD_CENTER',
+
+    'osu_fp'
 ]
